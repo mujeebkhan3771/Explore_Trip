5c1e4b683b067bd9d79ff135dfc4fc397151ef6f
Mujeeb3771 <mohammedmujeebkhan789@gmail.com>
2025-06-02 12:04:39 +0530

    Update app.jsc1

--- app.js
+++ app.js
@@ -108,7 +108,8 @@ app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
 app.get("/", (req, res) => {
-  res.send("Hi , I am a root.");
+  res.send("Hi ,My name is Mohammed Mujeeb Khan S, and I am currently pursuing my Bachelor's degree in Computer Science and Engineering with a specialization in Data Science (CSE-DS) at BITM college .
+           As an undergraduate student, I am passionate about exploring the field of data science and developing strong technical and analytical skills to solve real-world problems. ");
 });
 
 app.all("*", (req, res, next) => {
