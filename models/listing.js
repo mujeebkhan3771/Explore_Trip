bbf938139568a9b515d1446ec9093c9d21ad332e
Mujeeb3771 <mohammedmujeebkhan789@gmail.com>
2025-06-08 13:05:38 +0530

    Update listing.jsh1

--- models/listing.js
+++ models/listing.js
@@ -12,23 +12,22 @@ const listingSchema = new Schema({
     url: String,
     filename: String,
   },
-  // image: {
-  //   type: String,
-  //   default:
-  //     "https://plus.unsplash.com/premium_photo-1661964071015-d97428970584?q=80&w=1620&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  //   set: (v) =>
-  //     v === ""
-  //       ? "https://plus.unsplash.com/premium_photo-1661964071015-d97428970584?q=80&w=1620&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-  //       : v,
-  // },
-  price: Number,
+  timings: {
+    opening: {
+      type: String,
+      default: "9:30 AM",
+    },
+    closing: {
+      type: String,
+      default: "6:00 PM",
+    },
+  },
   location: String,
   country: String,
   reviews: [
     {
       type: Schema.Types.ObjectId,
       ref: "Review",
-      //reviews added
     },
   ],
   owner: {
@@ -37,8 +36,7 @@ const listingSchema = new Schema({
   },
 });
 
-// post mongoose middleware for handling deletions of reviews ==
-// > As if we delete Listing then the reviews should also be dleted in db
+// Post middleware to delete associated reviews when a listing is deleted
 listingSchema.post("findOneAndDelete", async (listing) => {
   if (listing) {
     await Review.deleteMany({ _id: { $in: listing.reviews } });
