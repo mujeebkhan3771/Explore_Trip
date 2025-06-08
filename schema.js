3eda70038b77d73b47a6b02559661c4fe41307fe
Mujeeb3771 <mohammedmujeebkhan789@gmail.com>
2025-06-08 13:17:40 +0530

    Update schema.jsh1

--- schema.js
+++ schema.js
@@ -1,13 +1,26 @@
 const Joi = require("joi");
 
+const timeFormat = /^([1-9]|1[0-2]):[0-5][0-9] (AM|PM)$/;
+
 module.exports.listingSchema = Joi.object({
   listing: Joi.object({
     title: Joi.string().required(),
     description: Joi.string().required(),
     location: Joi.string().required(),
     country: Joi.string().required(),
-    price: Joi.number().required().min(0),
     image: Joi.string().allow("", null),
+
+    timings: Joi.object({
+      opening: Joi.string().pattern(timeFormat).required()
+        .messages({
+          "string.pattern.base": `"opening" time must be in HH:MM AM/PM format (e.g., 9:30 AM)`,
+        }),
+      closing: Joi.string().pattern(timeFormat).required()
+        .messages({
+          "string.pattern.base": `"closing" time must be in HH:MM AM/PM format (e.g., 6:00 PM)`,
+        }),
+    }).required(),
+
   }).required(),
 });
 
